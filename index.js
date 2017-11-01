32beddee95b221358ae4675e01921fe1fc2868d6
harrischelsea <harix_bazdar@hotmail.com>
2017-11-01 18:48:17 +0100

    aaa

--- index.js
+++ index.js
@@ -1,26 +1,68 @@
 const net = require('net');
-const moment = require('moment');
-moment.locale('bs');
+var klijenti = [];
+var id = 0;
+
+
 const server = net.createServer((socket) => {
+    id++;
+    socket.mojeIme = '';
+
+    klijenti.push(socket);
+    socket.name = "Klijent" + id;
+
+    console.log(socket.name + ' se pridruzio razgovoru.');
 
     socket.on('data', (data) => {
-            console.log('poslao: ' + data);
+        const request = JSON.parse(data);
+        if (request.action === 'IME') {
+            console.log('Korisnik je registrovao ime ', request.value);
+            socket.mojeIme = request.value;
 
-            socket.write(data);
-        });
+            let sviOnline = klijenti.map(el => el.mojeIme);
+            socket.write(JSON.stringify(sviOnline));
 
-    socket.write('sta ima jos');
+        }
+        else if (request.action === 'PORUKA') {
+            console.log(request.name, ' je poslao poruku: ', request.value);
+            for (let i = 0; i < klijenti.length; i++) {
+                if (klijenti[i].mojeIme == request.receiver) {
+                    klijenti[i].write(request.name+ ': '+ request.value);
+                }
+            }
+        }
+        //console.log(socket.name + ' poslao: ' + data);
+
+        //poruka(socket, data);
+    });
+
+    var klijent = '';
+    socket.on('end', () => {
+        klijenti.splice(klijenti.indexOf(socket), 1);
+        klijent = socket.name;
+        poruka(socket, socket.name + ' se odjavio!');
+
+    });
+
+    socket.on('close', () => {
+        console.log(klijent + ' se diskonektovao');
+    });
 
 }).on('error', (err) => {
     // handle errors here
     throw err;
-});
+    });
 
 // grab an arbitrary unused port.
 server.listen({
-  port:80
+    port:80
 },() => {
     console.log('opened server on', server.address());
 });
 
-///////////////////////////////////////
+function poruka(salje, poruka){
+    for (let i = 0; i < klijenti.length; i++){
+        if (klijenti[i] != salje) {
+            klijenti[i].write(salje.name + ': ' + poruka);
+        }
+    }
+}
--- klijent.js
+++ klijent.js
@@ -1,4 +1,59 @@
 const net = require('net');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let mojeIme = '';
+let resiver = '';
+
+function pisi() {
+    rl.question('Reci nesto svima! \n', (answer) => {
+        // TODO: Log the answer in a database
+        console.log(`Rekli ste: ${answer}`);
+        if (answer.toString() === 'exit') {
+            client.destroy();
+            process.exit();
+        } else if (answer.toString() === 'close'){
+            privateChat();
+        } else {
+            const response = {
+                action: "PORUKA",
+                value: answer,
+                name: mojeIme,
+                receiver: resiver
+            }
+            client.write(JSON.stringify(response, null, 2));
+            pisi();
+        }
+    });
+}
+
+function ime(){
+    rl.question('Kako se zoves? \n', (ime) => {
+        // TODO: Log the answer in a database
+        console.log(`Rekli ste: ${ime}`);
+        const response = {
+          action: "IME",
+            value: ime
+        };
+        mojeIme = ime;
+        client.write(JSON.stringify(response, null, 2));
+        //pisi();
+        privateChat();
+    });
+}
+
+function privateChat() {
+    rl.question('S kim ces da pricas? \n', (ime) => {
+        // TODO: Log the answer in a database
+        console.log(`Rekli ste: ${ime}`);
+        resiver = ime;
+        pisi();
+        //pisi();
+    });
+}
 
 const client = new net.Socket();
 client.connect(80, '127.0.0.1', function() {
@@ -11,12 +66,20 @@ client.connect(80, '127.0.0.1', function() {
         //client.write(time2);
     }, 1000);
     */
+    ime();
 });
 
 client.on('data', (data) => {
-    console.log('klijent primio: ' + data);
+    console.log('klijent primio: ' + data + ' \n');
+    pisi();
 });
 
+
+client.on('close', () => {
+    client.write('Korisnik je napustio chat');
+});
+/*
 client.on('connect', () => {
     console.log('konektovani ste');
 });
+*/
